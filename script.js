cde0d9a4e991f414efc4ef37ce2eb204aad93e42
miwanoff <marina.iva.noffa@gmail.com>
2025-04-27 15:31:37 +0300

    Add color change

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 $(function () {
   $("#b1").click(function () {
-    alert("Hello");
+    // alert("Hello");
+    $("#p1").css("color", 'blue')
   });
+  
   $("#b2").click(function () {
-    alert("!!!!");
+    // alert("!!!!");
+    $("#demodiv").css({"background":'gold', 'padding':"10px"})
+
   });
+
+//   $("#p1").mouseenter(function(){
+//     alert("You entered p1!");
+//   });
+//   $("#p1").mouseleave(function(){
+//     alert("Bye! You now leave p1!");
+//   });
+// $("#p1").hover(function(){
+//     alert("You entered p1!");
+//   },
+//   function(){
+//     alert("Bye! You now leave p1!");
+//   });
+
+  $("#b3").click(function () {
+    $('#demodiv').animate({color: 'red'})
+  });
+
+
+
 });
